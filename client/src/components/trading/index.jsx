eb6010462469eb0038859203655d3384edf05cde
eli <ebheravi159@gmail.com>
2022-11-30 20:40:48 -0800

    feat: adding socket with auth and query

--- client/src/components/trading/index.jsx
+++ client/src/components/trading/index.jsx
@@ -5,6 +5,18 @@ import RenderLineChart from "./chart";
 const endPoint = "localhost:5000";
 let socket = io(endPoint);
 
+// const socket = io("ws://example.com/my-namespace", {
+//   reconnectionDelayMax: 10000,
+//   auth: {
+//     token: "123"
+//   },
+//   query: {
+//     "my-key": "my-value"
+//   }
+// });
+
+
+
 const Trading = () => {
     const [lastPrice, setLastPrice] = useState([]);
     const [isConnected, setIsConnected] = useState(false);
